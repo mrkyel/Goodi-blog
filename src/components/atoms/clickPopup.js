24c5ad05901d2a593442fe1c945e8e7c410b9602
mrkyel <kyel100@naver.com>
2019-10-29 09:59:51 +0900

    third fix

--- src/components/atoms/clickPopup.js
+++ src/components/atoms/clickPopup.js
@@ -1,4 +1,4 @@
-import React, { Component, Children } from "react";
+import React, { Component, children } from "react";
 import styled from "styled-components";
 import onClickOutside from "react-onclickoutside";
 
